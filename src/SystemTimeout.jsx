4f18823ea7af1d032596e1a3c573c30514e6df82
gurpreet singh <gurpreetsingh2@telus.com>
2021-05-27 19:21:44 +0530

    feat: test

--- src/SystemTimeout.jsx
+++ src/SystemTimeout.jsx
@@ -82,12 +82,7 @@ const SystemTimeOut = () => {
   
     // Otherwise, we need to ask the user for permission
     else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then((permission) => {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-          new Notification("you're logged in");
-        }
-      });
+      Notification.requestPermission()
     }
   
     // At last, if the user has denied notifications, and you
